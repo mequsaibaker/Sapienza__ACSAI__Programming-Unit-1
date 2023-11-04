acb8d79659ab840d39f2e25e04146ed121c30f01
me-qusaibaker <148033591+me-qusaibaker@users.noreply.github.com>
2023-11-04 08:30:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Write a function that gets a list of strings and returns a
+# dictionary with keys to all the characters found in the
+# strings of the list and, as values, a list of all the strings
+# that contain the character of the key. The lists in the
+# dictionary are sorted in alphabetical order.
+
+# Example: chars_n_words(['dog', 'hot', 'dot']) -> {'d':['dog', 'dot'], 'o':['dog', 'dot', 'hot'], 'g':['dog'], 'h':['hot'], 't':['dot', 'hot']}
+
+
+def chars_n_words(string_list):
+  pass
+
+
+print(chars_n_words(['dog', 'hot', 'dot']))
