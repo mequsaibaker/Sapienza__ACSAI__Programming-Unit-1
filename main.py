5f81827d85f8b72e369c533438ab07ce9eccd56b
me-qusaibaker <148033591+me-qusaibaker@users.noreply.github.com>
2023-11-08 12:34:20 +0000

    editing removeNewLines function and coding LongestWordInFile function

--- main.py
+++ main.py
@@ -17,15 +17,33 @@ def readTextInFile(filepath):
 
 
 def removeNewLines(txt):
-  txt.replace('\n', '')
-  return None
+  return txt.replace("\n", "Hi")
 
 
 def LongestWordInFile(filepath):
   # your code goes here
+  txt = removeNewLines(readTextInFile(filepath))
+  txt_len = len(txt)
+
+  max_word_len = 0
+  first_char_ind = 0
+  last_char_ind = 0
+
+  counter = 0
+  while counter < txt_len:
+    if first_char_ind.isalpha():
+      first_char_ind = counter
+      for ind in range(counter, txt_len):
+        last_char_ind = counter + ind
+        if not txt[last_char_ind].isalpha():
+          counter += last_char_ind
+          break
+      max_word_len = max(max_word_len, last_char_ind - first_char_ind)
   pass
 
 
 if __name__ == "__main__":
-  print(readTextInFile("testfile.txt"))
+  txt = readTextInFile("testfile.txt")
+  txt2 = txt.replace("\n", "Hi")
+  print(txt2)
   print(LongestWordInFile("testfile.txt"))
