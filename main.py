c1f0aa2e3b4973b67bdcb09867bb35bb6441bfee
me-qusaibaker <148033591+me-qusaibaker@users.noreply.github.com>
2023-11-08 18:09:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+write a function that takes a matrix (a list of lists) as input and returns the same matrix rotated by 90 degrees clockwise
+below is an example with random numbers to demonstrate its usage.
+
+input matrix:
+[
+    [8, 5, 2],
+    [9, 6, 3],
+    [7, 4, 1]
+]
+
+rotated matrix:
+[
+    [7, 9, 8],
+    [4, 6, 5],
+    [1, 3, 2]
+]
+
+'''
+
+
+def RotateMatrix(matrix):
+  # your code goes here
+  
+
+
+if __name__ == "__main__":
+  input_matrix = [[8, 5, 2], [9, 6, 3], [7, 4, 1]]
+
+  print(RotateMatrix(input_matrix))
