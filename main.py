f9b91a9572e9890f666ec63acb5d6747a963a5fa
me-qusaibaker <148033591+me-qusaibaker@users.noreply.github.com>
2023-11-08 13:39:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# look and read off the digits
+
+# write a function LookAndSay(n, k) that, starting from
+# the provided positive integer n, returns a list
+# of numbers, in which each number is obtained
+# by looking and saying the previous one, k times
+
+# note: the numbers are returned as strings
+
+# for example, given n=1 and k=8, the numbers are:
+
+# 1
+# 11
+# 21
+# 1211
+# 111221
+# 312211
+# 13112221
+# 1113213211
+# 31131211131221
+
+# and the function returns ["1", "11", "21", "1211", "111221",
+# "312211", "13112221", "1113213211", "31131211131221"]
+
+
+def LookAndSay(n, k):
+  # your code goes here
+  pass
+
+
+if __name__ == "__main__":
+  print(LookAndSay(1, 8))
