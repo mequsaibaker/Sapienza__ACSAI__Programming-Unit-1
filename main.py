e1244e3b856bd8fc00441547336945530cf7d466
me-qusaibaker <148033591+me-qusaibaker@users.noreply.github.com>
2023-12-01 13:39:00 +0000

    editing code

--- main.py
+++ main.py
@@ -24,40 +24,48 @@
 
 import pngmatrix
 
+
 def f1_LoadMario(filename: str) -> list:
   img = pngmatrix.load_png8(filename)
   return img
 
+
 def f2_ComputeLuigi(Mario: list) -> list:
-  for y in range(len(Mario)):
-    for x in range(len(Mario[y])):
-      if Mario[y][x] == (227, 6, 19):
-        Mario[y][x] = (0, 191, 99)
-  return Mario
+  Luigi = []
+  for row in Mario:
+    Luigi.append(row.copy())
+
+  for y in range(len(Luigi)):
+    for x in range(len(Luigi[y])):
+      if Luigi[y][x] == (227, 6, 19):
+        Luigi[y][x] = (0, 191, 99)
+  return Luigi
+
 
 def f3_MirrorLuigi(Luigi: list) -> None:
   pass
 
+
 def f4_GlueMarioAndLuigi(Mario: list, Luigi: list) -> list:
   pass
 
+
 def f5_DrawMarioAndLuigi(Mario: list, Luigi: list, bg_filename: str) -> list:
   pass
+
 
 def f6_RotateLuigi(Luigi: list) -> None:
   pass
 
 
-
-
 if __name__ == "__main__":
-    M = f1_LoadMario("Mario.png")
-    print(M)
-    # L = f2_ComputeLuigi(M)
-    # f3_MirrorLuigi(L)
-    # pngmatrix.save_png8(L, "Luigi_mirrored.png")
-    # M2 = f4_GlueMarioAndLuigi(M, L)
-    # pngmatrix.save_png8(M2, "MarioLuigi_glued.png")
-    # f5_DrawMarioAndLuigi(M, L, "background.png")
-    # f6_RotateLuigi(L)
-    # pngmatrix.save_png8(L, "Luigi_rotated.png")
+  M = f1_LoadMario("Mario.png")
+  print(M)
+  # L = f2_ComputeLuigi(M)
+  # f3_MirrorLuigi(L)
+  # pngmatrix.save_png8(L, "Luigi_mirrored.png")
+  # M2 = f4_GlueMarioAndLuigi(M, L)
+  # pngmatrix.save_png8(M2, "MarioLuigi_glued.png")
+  # f5_DrawMarioAndLuigi(M, L, "background.png")
+  # f6_RotateLuigi(L)
+  # pngmatrix.save_png8(L, "Luigi_rotated.png")
