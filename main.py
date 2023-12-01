7c97d11fa5b98b19917576b5cf855f294acb629b
me-qusaibaker <148033591+me-qusaibaker@users.noreply.github.com>
2023-12-01 15:00:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+write a function that is provided as input the path to a grayscale image; the function finds all the horizontal gray lines in the image (the image background is black) and returns a list of tuples (color, length) for each line found in the image, in which "color" is the (R, G, B) tuple representing the color of a line and "length" is the corresponding length; tuples must be sorted from the longest line to the shortest one, and, if several lines have the same length, by color intensity, from the brightest to the darkest.
+'''
+
+import pngmatrix
+
+def LongestLine(path_to_a_gray_image):
+  # your code goes here
+  pass
+    
+
+if __name__ == "__main__":
+  LongestLine("a grayscale image.png")
+
