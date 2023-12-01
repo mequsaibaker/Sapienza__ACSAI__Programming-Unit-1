9b54c71bac2cfc824a7081ded602304527b71c0e
me-qusaibaker <148033591+me-qusaibaker@users.noreply.github.com>
2023-12-01 09:59:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Write a function that gets two strings representing filenames input_file and output_file.
+# The input_file contains several rows with numbers separated by whitespaces and tabs.
+# The function must write in output_file the averages of the numbers found in every row,
+# in the same order and return the maximum computed average.
+
+# Example: if input_file has the following content:
+# 1 5 7
+# 3 1.4 12
+#
+# the function has to return the value 5.466666666666666 
+# and write the following lines in output_file:
+# 4.333333333333333
+# 5.466666666666666
+
+def averages_in_file(input_file, output_file):
+    
+    pass
+    
