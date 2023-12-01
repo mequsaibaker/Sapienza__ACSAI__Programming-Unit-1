8236b520ff5ac4de9eb2369a527fe7a3668d1dda
me-qusaibaker <148033591+me-qusaibaker@users.noreply.github.com>
2023-12-01 10:10:23 +0000

    implementing reader and find_average functions

--- main.py
+++ main.py
@@ -7,12 +7,32 @@
 # 1 5 7
 # 3 1.4 12
 #
-# the function has to return the value 5.466666666666666 
+# the function has to return the value 5.466666666666666
 # and write the following lines in output_file:
 # 4.333333333333333
 # 5.466666666666666
 
+
+def reader(input_file):
+  file_ref = open(input_file, 'r', encoding='utf8')
+  lines = file_ref.readlines()
+  file_ref.close()
+  return lines
+
+
+def find_average(lines):
+  num_list = []
+  num_sum = 0
+  num_avg = 0
+  for line in lines:
+    num_list = line.strip().split(' ')
+    for value in num_list:
+      num_sum += float(value)
+    num_avg = num_sum / len(num_list)
+  return num_avg
+
+
 def averages_in_file(input_file, output_file):
-    
-    pass
-    
+  lines = reader(input_file)
+  num_avg = find_average(lines)
+  return num_avg
