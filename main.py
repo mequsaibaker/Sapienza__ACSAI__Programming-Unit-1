40719e73a64a789e41962f39cb261ee197a17ae0
me-qusaibaker <148033591+me-qusaibaker@users.noreply.github.com>
2023-12-25 13:04:42 +0000

    implementing FindLineStartEnd function

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ import pngmatrix
 
 def FindLineStartEnd(single_row):
   # your code goes here
-  start = None
-  end = None
-  if end != None or len(single_row) == 0:
-    return None, None
+  if single_row == []:
+    return -1, -1, False, False
   else:
-    if single_row[0] != (0, 0, 0):
-      end = FindLineStartEnd(single_row[1:])
-      if start is None:
-        start = single_row[0]
-      return end + 1
-  pass
+    x_start, x_end, start_flag, end_flag = FindLineStartEnd(single_row[1:])
+    if not end_flag:
+      if single_row[0] != (0, 0, 0):
+        end_flag = True
+      return x_start - 1, x_end - 1, start_flag, end_flag
+    else:
+      if not start_flag:
+        if single_row[0] == (0, 0, 0) and single_row[1] != (0, 0, 0):
+          start_flag = True
+        return x_start - 1, x_end, start_flag, end_flag
+      else:
+        return x_start, x_end, start_flag, end_flag
 
 
 def LongestLine(path_to_a_gray_image):
@@ -28,10 +32,12 @@ def LongestLine(path_to_a_gray_image):
   max_len = 0
   img = pngmatrix.load_png8(path_to_a_gray_image)
   for row in img:
-    start, end = FindLineStartEnd(row)
-    max_len = max(max_len, end - start)
+    x_start, x_end, start_flag, end_flag = FindLineStartEnd(row)
+    # print(start, ' ', end)
+    max_len = max(max_len, x_end - x_start)
   return max_len
 
 
 if __name__ == "__main__":
-  LongestLine("a grayscale image.png")
+  print(LongestLine("a grayscale image.png"))
+  # print(FindLineStartEnd([1, (0, 0, 0), 2, 3, 4, 5, 6, 7]))
